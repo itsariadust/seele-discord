1666248ef4991932e1a8a490ff88a2c932d34e39
itsYuuki <jakobaindreas11@gmail.com>
2022-05-08 11:27:24 +0800

    Fix: Added filter if role is already present in db

--- src/commands/manager/addmod.ts
+++ src/commands/manager/addmod.ts
@@ -1,37 +1,42 @@
-import { Command } from '@sapphire/framework';
-import Guild from '../../models/guildSchema.js';
+import { Args, Command } from '@sapphire/framework';
+import type { Message } from 'discord.js';
+import GuildSettings from '../../models/guildSchema.js';
 
 export class AddModCommand extends Command {
-    constructor(context, options) {
+    constructor(context: Command.Context, options: Command.Options) {
         super(context, {
             ...options,
             name: 'addmod',
             description: 'Add moderator roles.',
             quotes: [],
-            preconditions: ['ownerOnly'],
+            preconditions: ['OwnerOnly'],
         });
     }
 
-    async messageRun(message, args) {
-        let guildSettings = await Guild.findOne({ guildID: message.guild.id });
+    async messageRun(message: Message, args: Args) {
         const settingValue = await args.pick('role').catch(() => null);
+        let guildSettings = await GuildSettings.findOne({ guildID: message.guild.id });
 
         if (settingValue === null) {
             return message.reply('You didn\'t specify a role to add as a moderator.');
         }
 
         if (!guildSettings) {
-            guildSettings = new Guild({
+            guildSettings = new GuildSettings({
                 guildName: message.guild.name,
                 guildID: message.guild.id,
                 modroles: settingValue.id,
             });
-            await guildSettings.save().catch(err => console.log(err));
+            await guildSettings.save().catch((err: any) => console.log(err));
             return message.reply(`I made an entry in the database for your server settings since you're a first time user. I added the role ${settingValue} to the database.`);
         }
 
+        if (guildSettings.modroles.includes(settingValue)) {
+            return message.reply('This role is already present.')
+        }
+
         guildSettings.modroles.push(settingValue.id);
-        await guildSettings.save().catch(err => console.log(err));
+        await guildSettings.save().catch((err: any) => console.log(err));
         return message.reply(`I added the role ${settingValue} to the database.`);
     }
 }
