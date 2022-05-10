59a7cbd10e114d45fcc0fd395ec90c1b385ae737
itsariadust <jakobaindreas11@gmail.com>
2022-05-11 01:40:56 +0800

    Added persistent cooldown to fix #1

--- src/commands/economy/daily.ts
+++ src/commands/economy/daily.ts
@@ -1,5 +1,5 @@
 import { Command } from '@sapphire/framework';
-import { Time } from '@sapphire/time-utilities';
+// import { Time } from '@sapphire/time-utilities';
 import type { Message } from 'discord.js';
 import Economy from '../../models/economySchema';
 
@@ -9,7 +9,6 @@ export class DailyCommand extends Command {
             ...options,
             name: 'daily',
             description: 'Receive $500 credits every day.',
-            cooldownDelay: Time.Day * 1,
         });
     }
 
@@ -18,8 +17,14 @@ export class DailyCommand extends Command {
         if (!wallet) {
             return message.reply('You don\'t have a wallet! Please use the `s!wallet create` command to make one.');
         }
+        const today = new Date();
+        if (wallet.nextDaily > today) {
+            return message.reply(`You still can't claim your dailies. Check back on ${wallet.nextDaily.toUTCString()}`);
+        }
         const newBalance = wallet.userBalance + 500;
+        const futureDate = new Date(Date.now() + (3600 * 1000 * 24));
         wallet.userBalance = newBalance;
+        wallet.nextDaily = futureDate.toUTCString();
         await wallet.save().catch((err: any) => console.log(err));
         return message.reply('You have received $500 credits!');
     }
--- src/models/economySchema.ts
+++ src/models/economySchema.ts
@@ -4,6 +4,7 @@ const economySchema = new mongoose.Schema({
     userID: String,
     userName: String,
     userBalance: Number,
+    nextDaily: Date,
 });
 
 export default mongoose.model('Economy', economySchema, 'economy');
