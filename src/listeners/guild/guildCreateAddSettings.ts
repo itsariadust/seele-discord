db6b8e0932974e3b66c57159cff962896fd69c20
itsariadust <jakobaindreas11@gmail.com>
2022-05-10 16:31:47 +0800

    Feature: Add guildCreate event that makes a new document when bot joins the server for the first time.

---
+++ src/listeners/guild/guildCreateAddSettings.ts
@@ -0,0 +1,28 @@
+import GuildSettings from '../../models/guildSchema';
+import { Listener, container } from '@sapphire/framework';
+import type { Guild } from 'discord.js';
+
+export class UserEvent extends Listener {
+    public constructor(context: Listener.Context, options: Listener.Options) {
+        super(context, {
+            ...options,
+            event: 'guildCreate',
+        });
+    }
+
+    public async run(guild: Guild) {
+        console.log('Event fired.');
+        let guildSettings = await GuildSettings.findOne({ guildID: guild.id });
+        if (!guildSettings) {
+            guildSettings = new GuildSettings({
+                guildName: guild!.name,
+                guildID: guild!.id,
+                prefix: 's!',
+            });
+            await guildSettings.save().catch((err: any) => console.log(err));
+            return container.logger.info(`Bot entered Guild ${guild.name} (${guild.id}) for the first time.`);
+        }
+
+        return container.logger.info(`Bot entered Guild ${guild.name} (${guild.id}).`);
+    }
+}
